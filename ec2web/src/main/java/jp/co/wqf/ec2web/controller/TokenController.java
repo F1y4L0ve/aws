5a7bcda9b794b9ae8590a0eb1f9179773619d02d
gikeihi <gikeihi@yahoo.co.jp>
2018-06-26 23:20:11 +0900

    modify request credential method.

--- ec2web/src/main/java/jp/co/wqf/ec2web/controller/TokenController.java
+++ ec2web/src/main/java/jp/co/wqf/ec2web/controller/TokenController.java
@@ -8,8 +8,8 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
 import com.amazonaws.services.securitytoken.model.Credentials;
-import com.amazonaws.services.securitytoken.model.GetSessionTokenRequest;
 import com.amazonaws.services.securitytoken.model.GetSessionTokenResult;
 
 @RestController
@@ -21,10 +21,13 @@ public class TokenController {
 		AWSCredentialsProvider cp = new InstanceProfileCredentialsProvider(false);
 		// AWSCredentialsProvider cp = new ProfileCredentialsProvider();
 		AWSSecurityTokenService sts = AWSSecurityTokenServiceClientBuilder.standard().withCredentials(cp).build();
-		GetSessionTokenResult sessionToken = sts
-				.getSessionToken(new GetSessionTokenRequest().withDurationSeconds(1800));
+		sts.assumeRole(new AssumeRoleRequest().withRoleArn("arn:aws:iam::270221123016:role/Role4S3")
+				.withRoleSessionName("test").withDurationSeconds(3600));
+		// GetSessionTokenResult sessionToken = sts
+		// .getSessionToken(new GetSessionTokenRequest().withDurationSeconds(1800));
+		// Credentials credentials = sessionToken.getCredentials();
+		GetSessionTokenResult sessionToken = sts.getSessionToken();
 		Credentials credentials = sessionToken.getCredentials();
-
 		return credentials;
 	}
 }
